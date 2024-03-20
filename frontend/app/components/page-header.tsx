06ffcd83e1d3a2aeb958370c73bca48c8d0c8dff
alan <alanjames6282@gmail.com>
2024-03-21 00:39:57 +0530

    add homepage link and style to page header

--- frontend/app/components/page-header.tsx
+++ frontend/app/components/page-header.tsx
@@ -11,7 +11,7 @@ export default function PageHeader() {
 
     return (
         <nav className="flex flex-row items-center h-16 px-4 border-b border-gray-200/50 backdrop-filter backdrop-blur-sm bg-gray-50 dark:bg-gray-950/60">
-            <Link className="flex items-center space-x-2 text-sm font-medium mr-4 sm:mr-10 lg:mr-14" href="#">
+            <Link className={`flex items-center space-x-2 text-sm font-medium mr-4 sm:mr-10 lg:mr-14 ${pathname == '/' ? 'pointer-events-none' : ''}`}  href={pathname == '/' ? '#' : '/'}>
                 <span className=" font-bold  text-2xl">takeUforward</span>
             </Link>
             <nav className="hidden space-x-4 lg:flex flex-row text-base font-medium">
