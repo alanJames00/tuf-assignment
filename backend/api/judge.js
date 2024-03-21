936373b354cd12cbbff59a2596f233dad3b34d1b
alan <alanjames6282@gmail.com>
2024-03-21 14:03:51 +0530

    fix judge0 api issues, add routes to run code

---
+++ backend/api/judge.js
@@ -0,0 +1,60 @@
+// interacts with the judge0 API
+const axios = require('axios');
+
+async function runCode({ stdin, langId, sourceCode }) {
+
+  const submitOptions = {
+    method: 'POST',
+    url: 'https://judge0-ce.p.rapidapi.com/submissions',
+    params: {
+      base64_encoded: 'true',
+      fields: '*'
+    },
+    headers: {
+      'content-type': 'application/json',
+      'Content-Type': 'application/json',
+      'X-RapidAPI-Key': process.env.JUDGE_API_KEY,
+      'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+      },
+    data: {
+      language_id: langId,
+      source_code: sourceCode,
+      stdin: stdin,
+      }
+  };
+
+  try {
+	  const submitResp = await axios.request(submitOptions);
+	  console.log(submitResp.data);
+
+    const submitToken = submitResp.data.token;
+
+    // then get the submission using token
+    console.log(submitToken);
+    
+    const getOptions = {
+    method: 'GET',
+    url: `https://judge0-ce.p.rapidapi.com/submissions/${submitToken}`,
+    params: {
+      base64_encoded: 'true',
+      fields: '*'
+    },
+    headers: {
+      'X-RapidAPI-Key': '4aa3d62f38msh26169a9b3653ca1p16d1f9jsn9b81366aeea9',
+      'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+    }
+  };
+
+    const getResp = await axios.request(getOptions);
+	  console.log(getResp.data);
+    return getResp.data;
+
+  } catch (error) {
+	  console.error(error);
+  }
+}
+
+
+module.exports = {
+  runCode
+}
--- backend/api/routes.js
+++ backend/api/routes.js
@@ -1,8 +1,70 @@
 const express = require('express');
 const apiRouter = express.Router();
+const pool = require('./db');
+const judge = require('./judge');
 
-apiRouter.post('/t', (req, res) => {
-  console.log(req.body);
+// List all the submissions
+apiRouter.get('/submissions', async (req, res) => {
+
+  try {
+    const [result, fields] = await pool.promise().query('SELECT * FROM submission');
+    console.log(fields);
+  }
+  catch(e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+
+
+// fetch submission by id
+apiRouter.get('/submission/:id', async (req, res) => {
+
+  try {
+
+  }
+  catch(e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+
+
+apiRouter.post('/run', async (req, res) => {
+  
+  try {
+    // run the code and return the result
+    //
+    const { username, language, code, stdin } = req.body;
+    console.log(req.body);
+
+    const result = await judge.runCode({
+      stdin: stdin,
+      langId: language,
+      sourceCode: code,
+    });
+
+    res.json(result);
+  }
+  catch(e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
 })
+
+// execute and submit the code
+apiRouter.post('/submit', (req, res) => {
+
+  // run the code, save insert result to db and return result to user
+  try {
+    const { username, language, code, stdin } = req.body;
+    console.log(req.body);
+
+  }
+  catch(e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
 
 module.exports = apiRouter;
